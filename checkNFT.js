d99d84cf5fb3821dde208481854a5c95f53dc6b9
narovel <152124552+narovel@users.noreply.github.com>
2023-11-28 01:16:01 -0300

    Update checkNFT.js
    adicionando console.log(web3) para testar se o objeto web3 existe e está definido

--- checkNFT.js
+++ checkNFT.js
@@ -1,3 +1,4 @@
+console.log(web3);
 async function checkNFT(web3, contractAddress, ownerAddress, tokenId) {
     const abi = [
 	{
@@ -30,7 +31,7 @@ async function checkNFT(web3, contractAddress, ownerAddress, tokenId) {
 		"type": "function"
 	}
 ]; // Substitua isso pela ABI do seu contrato
-    const contract = new web3.eth.Contract(abi, contractAddress)
+    const contract = new web3.eth.Contract(abi, contractAddress);
     const result = await contract.methods.checkNFT(contractAddress, ownerAddress, tokenId).call();
     return result;
 }
