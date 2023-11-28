028edb9ac2fbefd1160eaa307049db832fb2ffd5
narovel <152124552+narovel@users.noreply.github.com>
2023-11-27 23:23:36 -0300

    Update checkNFT.js
    adicionado ABI

--- checkNFT.js
+++ checkNFT.js
@@ -1,5 +1,35 @@
 async function checkNFT(web3, contractAddress, ownerAddress, tokenId) {
-    const abi = []; // Substitua isso pela ABI do seu contrato
+    const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_contract",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "checkNFT",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]; // Substitua isso pela ABI do seu contrato
     const contract = new web3.eth.Contract(abi, contractAddress);
     const result = await contract.methods.checkNFT(contractAddress, ownerAddress, tokenId).call();
     return result;
