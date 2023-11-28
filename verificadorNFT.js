87779d325099c614cf3dd54415b41d9d7beb6085
narovel <152124552+narovel@users.noreply.github.com>
2023-11-28 20:11:54 -0300

    Update verificadorNFT.js

--- verificadorNFT.js
+++ verificadorNFT.js
@@ -597,7 +597,7 @@ const verificarNFT = async () => {
 		"stateMutability": "nonpayable",
 		"type": "function"
 	}
-]
+];
 	// Crie a instância do contrato ERC-721
         const contratoNFT = new web3.eth.Contract(abiContratoNFT, contratoNFTAddress);
 
